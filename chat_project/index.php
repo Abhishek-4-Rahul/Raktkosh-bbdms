85e93307967737e4c603e4d67a04b50b411feeff
Abhishek Adheesh <74251052+Abhishek-4-Rahul@users.noreply.github.com>
2021-04-22 09:03:56 +0530

    Add files via upload
    Initial Commit

---
+++ chat_project/index.php
@@ -0,0 +1,12 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:red; margin-top:5%">Welcome to Raktkosh's Chatbox</h2></center>
+</div>
+<?php include('layouts/footer2.php');?>
+</footer><script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
