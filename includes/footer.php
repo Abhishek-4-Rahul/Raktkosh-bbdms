48279a9af7e60c0782baf97923c97ed555a3765d
Abhishek Adheesh <74251052+Abhishek-4-Rahul@users.noreply.github.com>
2021-04-22 09:06:15 +0530

    Add files via upload
    Initial Commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white"> Raktkosh By Group-1:- 2017-21</p>
+        </div>
+        <!-- /.container -->
+    </footer><script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="firstpage.php">Raktkosh</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=aboutus">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="patient.php?type=donor">Patient Request</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Be a Donar</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search-donor.php">Search Blood</a>
+                    </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Feedback</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php ">Logout</a>
+                 
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
